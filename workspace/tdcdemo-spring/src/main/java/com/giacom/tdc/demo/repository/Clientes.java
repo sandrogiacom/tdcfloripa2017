f27fc61141f337d41fd417c778ebf3e598929472
sandro.giacomozzi <sandrogiacom@gmail.com>
2017-04-20 19:09:11 -0300

    remove workspace dir

--- workspace/tdcdemo-spring/src/main/java/com/giacom/tdc/demo/repository/Clientes.java
+++
@@ -1,9 +0,0 @@
-package com.giacom.tdc.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.giacom.tdc.demo.model.Cliente;
-
-public interface Clientes extends JpaRepository<Cliente, Long> { 
-
-}
